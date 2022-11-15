1a4e8a6445e4e4645307bdfe4faee0e6b10a8faf
hamza ennaji <80819463+hamzaennaji@users.noreply.github.com>
2022-11-15 05:55:06 +0100

    Add files via upload

---
+++ final project/client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'oaeqpvzhkl';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-4q0jddfmvoebgbq2.us.auth0.com',
+  clientId: 'LCLNb2Iv8XVLTcABBT5kS3YfBT4t3rCq',
+  callbackUrl: 'http://localhost:3000/callback'
+};
